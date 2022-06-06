771027f7bd9a891680cdf1b0c8c2c6c4b7c1aa4e
ISR404 <1.6.2mine@gmail.com>
2022-06-06 21:28:46 +0700

    Prototype is ready. Preparing for Unit Test coverage.

--- Impressions_app/urls.py
+++ Impressions_app/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('global_map/', views.global_map, name='global_map'),
-    path('test_output/', views.test_output, name='test_output'),
     path('create_marker/', views.create_marker, name='create_marker'),
     path('logout/', views.logout_view, name='logout')
 ]
