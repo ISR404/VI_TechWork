f2657a69d81c07918c5885fb1f7592b32fd87e42
ISR404 <1.6.2mine@gmail.com>
2022-06-07 02:52:15 +0700

    Cleaning database, translating UI on English, Fixed bug with authorization

--- Impressions_app/pipeline.py
+++ Impressions_app/pipeline.py
@@ -1,8 +1,15 @@
-def get_avatar(backend, response, user=None, *args, **kwargs):
+from Impressions_app.models import Profile
 
+
+def get_avatar(backend, response, user=None, *args, **kwargs):
     url = None
 
+    if user != None:
+        user_profile = Profile.objects.get_or_create(user=user)
+        user.profile = user_profile[0]
+
     if backend.name == 'vk-oauth2':
+
         url = response.get('photo', '')
 
     if url:
--- VI_TechWork/settings.py
+++ VI_TechWork/settings.py
@@ -121,9 +121,9 @@ SOCIAL_AUTH_VK_OAUTH2_SECRET = 'iMD0V4kWWkInLCzUczOi'
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
